c876526c4bdae03b3a651d2bdb828f15dbf4dd31
elvispresleyconceicao <ramospresley@gmail.com>
2026-04-12 18:36:53 -0300

    atualização de arquivos

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
     // ==========================================
     // SUAS CHAVES DA ULTRAMSG
     // ==========================================
-    const instanceId = process.env.ULTRAMSG_TOKEN; // Substitua pelo seu ID
-    const token = process.env.ULTRAMSG_INSTANCE; // Substitua pelo seu Token
+    const instanceId = "instance169794"; // Substitua pelo seu ID
+    const token = "faf6azuueplzkv0t"; // Substitua pelo seu Token
 
     // A URL oficial da UltraMsg que dispara mensagens
     const urlApiWhatsApp = `https://api.ultramsg.com/${instanceId}/messages/chat`;
